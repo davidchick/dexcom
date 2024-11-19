34611e2ff71aec51b34238785fd846169c506abc
David Chick <dchick@mac.com>
2024-11-18 20:06:03 -0800

    added some query params

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const dexButton = document.getElementsByClassName('dexcom')[0];
 let counter = 0;
 
+let initialURL = '';
+let queryString = '';
+const mySearchParams = new URLSearchParams(window.location.search);
+const myCode = mySearchParams.get('code');
+
+const dexEnvs = {
+    sandbox: 'https://sandbox-api.dexcom.com',
+    prod: 'https://api.dexcom.com',
+};
+
+const initialQuery = {
+    client_id: myAppInfo.client_id,
+    redirect_uri: myAppInfo.redirect_uri,
+    response_type: 'code',
+    scope: 'offline_access',
+};
+
+let qProperties = Object.getOwnPropertyNames(initialQuery);
+
+initialURL = `${dexEnvs.sandbox}/v2/oauth2/login?`;
+
+qProperties.forEach(element => {
+    queryString = `${queryString}${element}=${initialQuery[element]}&`;
+});
+
+dexButton.href = `${initialURL}${queryString}`;
+
 const callDexcom = function (e) {
 
     if (counter < 1) {
@@ -14,4 +41,32 @@ const callDexcom = function (e) {
 
 dexButton.addEventListener('click', e => {callDexcom()});
 
+console.log(myCode);
 
+const formData = {
+    grant_type: 'authorization_code',
+    code: myCode,
+    redirect_uri: myAppInfo.redirect_uri,
+    client_id: myAppInfo.client_id,
+    client_secret: myAppInfo.client_secret
+};
+
+if (counter) {
+
+    const resp = await fetch(
+        `${dexEnvs.sandbox}/v2/oauth2/token`,
+        {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams(formData).toString()
+        }
+    );
+  
+    const data = await resp.text();
+    console.log(data);
+
+};
+
+ 
